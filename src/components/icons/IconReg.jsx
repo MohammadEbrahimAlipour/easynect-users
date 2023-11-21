2b4bee0e281ef08ab2b8803f3f270660a37f7f48
Neda Aynechi <neda1995aynechi@gmail.com>
2023-11-21 10:30:34 +0330

    styles added to landing/login/register

--- src/components/icons/IconReg.jsx
+++ src/components/icons/IconReg.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { CardsCardIcon, CardsWifiIcon } from "../Icons";
 
 const IconReg = () => {
   return (
@@ -10,21 +11,23 @@ const IconReg = () => {
             {/* two items left side */}
             <div className=" absolute left-5 top-5">
               {/* circle */}
-              <div className="w-[28px] h-[28px] bg-mutedDark opacity-[17%] rounded-full " />
+              <div className="w-[28px] h-[28px] bg-dark  rounded-full flex justify-center items-center">
+                <CardsWifiIcon />
+              </div>
               <div
-                className="w-[28px] h-[24px] bg-mutedDark opacity-[17%] rounded-md mt-3 flex flex-col
+                className="w-[28px] h-[24px] rounded-md mt-3 flex flex-col
         justify-start "
               >
-                <div className="w-full h-[40%] rounded-se-md rounded-ss-md bg-dark"></div>
+                <CardsCardIcon />
               </div>
             </div>
             <div className="">
               {/* profile photo */}
               <div className="w-[50px] h-[50px] bg-dark rounded-full opacity-80 mb-4" />
               {/* box under profile */}
-              <div className="w-[153px] h-[14px] bg-mutedDark rounded-lg opacity-[35%] mb-2" />
+              <div className="w-[123px] h-[12px] bg-mutedDark rounded-lg opacity-[35%] mb-2" />
               {/* smaller line */}
-              <div className="w-[100px] h-[10px] bg-mutedDark rounded-lg opacity-20 " />
+              <div className="w-[100px] h-[9px] bg-mutedDark rounded-lg opacity-20 " />
             </div>
           </div>
         </div>
