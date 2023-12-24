96a279aafebb641c7be047188422729563cbca90
Ebrahim Alipour <mohammadebrahimalipour1377@gmail.com>
2023-12-24 17:23:38 +0000

    debugging

--- next.config.js
+++ next.config.js
@@ -9,8 +9,8 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: [
-      process.env.S3_PAGES_URL,
-      process.env.S3_CONTENTS_URL
+      process.env.NEXT_PUBLIC_S3_PAGES_URL,
+      process.env.NEXT_PUBLIC_S3_CONTENTS_URL
     ]
   }, 
   output: 'standalone'
--- src/components/ApiUr.js
+++ src/components/ApiUr.js
@@ -1,7 +1,7 @@
 // Define a function to generate the dynamic API URL
 export const generateApiUrl = (endpoint) => {
   // Replace this base URL with your dynamic logic if needed
-  const baseUrl = process.env.BACKEND_SERVER;
+  const baseUrl = process.env.NEXT_PUBLIC_BACKEND_SERVER;
   // const baseUrl = "http://188.121.115.0:8000";
   // const baseUrl = "https://a01f-83-123-72-196.ngrok-free.app";
   return `${baseUrl}${endpoint}`;
