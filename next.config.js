74a81f0f1c604ce83d17639acb47a70b76b4943c
Neda Aynechi <neda1995aynechi@gmail.com>
2023-12-25 10:58:47 +0330

    last update monday

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true
-// };
-
-// module.exports = nextConfig;
 
 const nextConfig = {
   reactStrictMode: true,
@@ -12,20 +7,8 @@ const nextConfig = {
       process.env.NEXT_PUBLIC_S3_PAGES_URL,
       process.env.NEXT_PUBLIC_S3_CONTENTS_URL
     ]
-  }, 
-  output: 'standalone'
+  },
+  output: "standalone"
 };
 
 module.exports = nextConfig;
-
-// const nextConfig = {
-//   reactStrictMode: true,
-//   images: {
-//     domains: [
-//       "easynect-pages-contents.s3.ir-thr-at1.arvanstorage.ir"
-
-//     ]
-//   }
-// };
-
-// module.exports = nextConfig;
