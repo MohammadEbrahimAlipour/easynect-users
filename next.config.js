d264cf609d4c004e5dc39c2752cd3464188d8fe7
Neda Aynechi <neda1995aynechi@gmail.com>
2024-01-22 15:14:47 +0330

    icon

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  reactStrictMode: false,
+  reactStrictMode: true,
   images: {
     domains: [
       process.env.NEXT_PUBLIC_S3_PAGES_URL,
