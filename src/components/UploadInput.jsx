03446b3bfd40110ca14b989d63661e3c90e9f52d
Mussy <mostafasalehi796@gmail.com>
2024-05-30 18:53:39 +0330

    feat: add progress bar to Upload Input

--- src/components/UploadInput.jsx
+++ src/components/UploadInput.jsx
@@ -8,11 +8,14 @@ export default function UploadInput({
   onChoose,
   labelText = null,
   className = "",
+  uploadPercentage = null,
 }) {
   const [fileName, setFileName] = useState(null);
   const fileInputRef = useRef();
 
   const handleFileInputClick = () => {
+    if (uploadPercentage !== null) return;
+
     fileInputRef.current.click();
   };
 
@@ -32,8 +35,15 @@ export default function UploadInput({
       {labelText && <Label>{labelText}</Label>}
       <InputWrapper onClick={handleFileInputClick}>
         <Input type="file" ref={fileInputRef} onChange={handleFileChange} />
-        <IconWrapper>
-          <Icon />
+        <IconWrapper $uploading={uploadPercentage !== null}>
+          {uploadPercentage !== null ? (
+            <>
+              <Percentage>{uploadPercentage}%</Percentage>
+              <Loader />
+            </>
+          ) : (
+            <Icon />
+          )}
         </IconWrapper>
         <Placeholder>
           {fileName ? fileName : "برای انتخاب فایل کلیک کنید"}
@@ -69,6 +79,14 @@ const IconWrapper = tw.div`
   bg-black
   rounded-md
   px-1
+  relative
+
+  ${({ $uploading }) =>
+    $uploading &&
+    `
+    rounded-full 
+    scale-110
+  `}
 `;
 
 const Icon = tw(UploadFileIcon)`
@@ -88,4 +106,29 @@ const Label = tw.p`
   text-muted
   whitespace-nowrap
   my-2
+`;
+
+const Percentage = tw.div`
+  text-white
+  text-xs
+  w-8
+  h-8
+  overflow-hidden
+  text-center
+  leading-9
+  scale-[90%]
+`;
+
+const Loader = tw.div`
+  top-0
+  left-0
+  absolute
+  border-4
+  border-t-4
+  border-gray-300
+  border-t-blue-500
+  rounded-full
+  w-10
+  h-10
+  animate-spin
 `;
